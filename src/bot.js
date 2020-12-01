d7612a2d046069e543f7d7c8f93e978ad5117fc9
PhuMGT <phu@millergoldtech.com>
2020-12-01 16:15:15 +0700

    create app ms team

--- src/bot.js
+++ src/bot.js
@@ -7,6 +7,7 @@ import {
     MemoryStorage,
     ConversationState,
     TurnContext,
+    CardFactory,
 } from 'botbuilder';
 import config from 'config';
 
@@ -45,9 +46,87 @@ export class EchoBot extends TeamsActivityHandler {
     constructor() {
         super();
         this.onMessage(async (context, next) => {
+            // console.log(context);
             TurnContext.removeRecipientMention(context.activity);
             const text = context.activity.text.trim().toLocaleLowerCase();
-            await context.sendActivity('You said ' + text);
+            // await context.sendActivity('You said ' + text);
+            if (text.includes('questions')) {
+                await this.runQuestion(context);
+            }
         });
+    }
+    async runQuestion(context) {
+        const card = {
+            type: 'AdaptiveCard',
+            body: [
+                {
+                    type: 'TextBlock',
+                    text: 'summary',
+                    size: 'Large',
+                    weight: 'Bolder',
+                    wrap: true,
+                },
+                {
+                    type: 'TextBlock',
+                    text: ' ${location} ',
+                    isSubtle: true,
+                    wrap: true,
+                },
+                {
+                    type: 'TextBlock',
+                    text:
+                        "${formatDateTime(start.dateTime, 'HH:mm')} - ${formatDateTime(end.dateTime, 'hh:mm')}",
+                    isSubtle: true,
+                    spacing: 'None',
+                    wrap: true,
+                },
+                {
+                    type: 'TextBlock',
+                    text: 'Snooze for',
+                    wrap: true,
+                },
+                {
+                    type: 'Input.ChoiceSet',
+                    id: 'snooze',
+                    value: '${reminders.overrides[0].minutes}',
+                    choices: [
+                        {
+                            $data: '${reminders.overrides}',
+                            title: '${minutes} minutes',
+                            value: '${minutes}',
+                        },
+                    ],
+                },
+            ],
+            actions: [
+                {
+                    type: 'Action.Submit',
+                    title: 'Snooze',
+                    data: {
+                        x: 'snooze',
+                    },
+                },
+                {
+                    type: 'Action.Submit',
+                    title: "I'll be late",
+                    data: {
+                        x: 'late',
+                    },
+                },
+            ],
+            version: '1.0.0',
+        };
+        const activeCard = CardFactory.adaptiveCard(card);
+        await context.sendActivity(
+            'Please give a minutes to answer the questions!'
+        );
+        // await context.sendActivity(activeCard);
+        return {
+            composeExtension: {
+                type: 'result',
+                attachmentLayout: 'list',
+                attachments: [activeCard],
+            },
+        };
     }
 }
--- src/message-extension.js
+++ src/message-extension.js
@@ -1,17 +1,15 @@
 import { TeamsActivityHandler, CardFactory } from 'botbuilder';
 import faker from 'faker';
 
-
-
 export default class MessageExtension extends TeamsActivityHandler {
     async handleTeamsMessagingExtensionQuery(context, query) {
-
         // If the user supplied a title via the cardTitle parameter then use it or use a fake title
-        let title = query.parameters && query.parameters[0].name === 'cardTitle'
-            ? query.parameters[0].value
-            : faker.lorem.sentence();
+        let title =
+            query.parameters && query.parameters[0].name === 'cardTitle'
+                ? query.parameters[0].value
+                : faker.lorem.sentence();
 
-        let randomImageUrl = "https://loremflickr.com/200/200"; // Faker's random images uses lorempixel.com, which has been down a lot
+        let randomImageUrl = 'https://loremflickr.com/200/200'; // Faker's random images uses lorempixel.com, which has been down a lot
 
         switch (query.commandId) {
             case 'getRandomText':
@@ -20,23 +18,106 @@ export default class MessageExtension extends TeamsActivityHandler {
                 // Generate 5 results to send with fake text and fake images
                 for (let i = 0; i < 5; i++) {
                     let text = faker.lorem.paragraph();
-                    let images =  [`${randomImageUrl}?random=${i}`]
-                    let thumbnailCard = CardFactory.thumbnailCard(title, text, images)
-                    let preview = CardFactory.thumbnailCard(title, text, images)
-                    preview.content.tap = { type: 'invoke', value: { title, text, images } };
-                    var attachment = { ...thumbnailCard, preview }
+                    let images = [`${randomImageUrl}?random=${i}`];
+                    let thumbnailCard = CardFactory.thumbnailCard(
+                        title,
+                        text,
+                        images
+                    );
+                    let preview = CardFactory.thumbnailCard(
+                        title,
+                        text,
+                        images
+                    );
+                    preview.content.tap = {
+                        type: 'invoke',
+                        value: { title, text, images },
+                    };
+                    var attachment = { ...thumbnailCard, preview };
                     attachments.push(attachment);
-
                 }
 
                 return {
                     composeExtension: {
                         type: 'result',
                         attachmentLayout: 'list',
-                        attachments: attachments
-                    }
+                        attachments: attachments,
+                    },
                 };
+            case 'getRandomQuestion':
+                let cards = [];
 
+                for (let i = 0; i < 5; i++) {
+                    const card = {
+                        type: 'AdaptiveCard',
+                        body: [
+                            {
+                                type: 'TextBlock',
+                                text: 'summary',
+                                size: 'Large',
+                                weight: 'Bolder',
+                                wrap: true,
+                            },
+                            {
+                                type: 'TextBlock',
+                                text: ' ${location} ',
+                                isSubtle: true,
+                                wrap: true,
+                            },
+                            {
+                                type: 'TextBlock',
+                                text:
+                                    "${formatDateTime(start.dateTime, 'HH:mm')} - ${formatDateTime(end.dateTime, 'hh:mm')}",
+                                isSubtle: true,
+                                spacing: 'None',
+                                wrap: true,
+                            },
+                            {
+                                type: 'TextBlock',
+                                text: 'Snooze for',
+                                wrap: true,
+                            },
+                            {
+                                type: 'Input.ChoiceSet',
+                                id: 'snooze',
+                                value: '${reminders.overrides[0].minutes}',
+                                choices: [
+                                    {
+                                        $data: '${reminders.overrides}',
+                                        title: '${minutes} minutes',
+                                        value: '${minutes}',
+                                    },
+                                ],
+                            },
+                        ],
+                        actions: [
+                            {
+                                type: 'Action.Submit',
+                                title: 'Snooze',
+                                data: {
+                                    x: 'snooze',
+                                },
+                            },
+                            {
+                                type: 'Action.Submit',
+                                title: "I'll be late",
+                                data: {
+                                    x: 'late',
+                                },
+                            },
+                        ],
+                        version: '1.0.0',
+                    };
+                    const activeCard = CardFactory.adaptiveCard(card);
+                    cards.push(activeCard);
+                }
+                return {
+                    composeExtension: {
+                        type: 'result',
+                        attachmentLayout: 'list',
+                        attachments: cards,
+                    },
+                };
             default:
                 break;
         }
@@ -44,14 +125,14 @@ export default class MessageExtension extends TeamsActivityHandler {
     }
 
     async handleTeamsMessagingExtensionSelectItem(context, obj) {
-        const { title, text, images } = obj
+        const { title, text, images } = obj;
 
         return {
             composeExtension: {
                 type: 'result',
                 attachmentLayout: 'list',
-                attachments: [CardFactory.thumbnailCard(title, text, images)]
-            }
+                attachments: [CardFactory.thumbnailCard(title, text, images)],
+            },
         };
     }
 }
